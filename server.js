58ffa23a584f3e4bcdb291d69dc1bc9ffdef5bf9
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-03-20 10:58:45 -0300

    Adds an endpoint of graphql

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: process.env.NODE_ENV === 'development',
+}));
 
 app.listen(4000, () => {
   console.log('Listening');
